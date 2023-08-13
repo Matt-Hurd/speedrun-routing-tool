a6e94435f7ff49318c113ace7729f6cf2e39e882
Matt-Hurd <sabahbob@gmail.com>
2023-08-13 04:04:07 -0500

    ProgressDisplay: Skip warp/activate shrines, better formatting
    models export Shrine
    
    Add zelda fonts

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,5 +2,5 @@ export { default as Game } from "./Game";
 export { default as Route } from "./Route";
 export { default as Branch } from "./Branch";
 export { default as Point } from "./Point";
-export { default as Thing, Korok, Item } from "./Thing";
+export { default as Thing, Korok, Item, Shrine } from "./Thing";
 export { default as Layer } from "./Layer";
