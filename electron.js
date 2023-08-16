9e5a66412ea3ad225ee9a53425ff599313476dfe
Matt-Hurd <sabahbob@gmail.com>
2023-08-16 04:16:38 -0500

    Updates

--- electron.js
+++ electron.js
@@ -1,41 +1,56 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
-const isDev = require('electron-is-dev');
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+const isDev = require("electron-is-dev");
+const {
+  installExtension, REACT_DEVELOPER_TOOLS
+} = require("electron-extension-installer");
 
 try {
-    require('electron-reloader')(module)
+  require("electron-reloader")(module);
 } catch (_) {}
 
 let mainWindow;
 
 function createWindow() {
-    mainWindow = new BrowserWindow({
-        width: 900,
-        height: 680,
-        webPreferences: {
-            nodeIntegration: true
-        }
-    });
+  mainWindow = new BrowserWindow({
+    width: 1920,
+    height: 1080,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
 
-    mainWindow.loadURL(
-        isDev
-            ? 'http://localhost:3000'  // Assuming you run your dev server on this port
-            : `file://${path.join(__dirname, '../build/index.html')}`
-    );
+  if (isDev) {        
+    mainWindow.webContents.once("dom-ready", async () => {
+    await installExtension(REACT_DEVELOPER_TOOLS)
+        .then((name) => console.log(`Added Extension:  ${name}`))
+        .catch((err) => console.log("An error occurred: ", err))
+        .finally(() => {
+          mainWindow.webContents.openDevTools();
+        });
+});
+  }
 
-    mainWindow.on('closed', () => (mainWindow = null));
+  mainWindow.loadURL(
+    isDev
+      ? "http://localhost:3000" // Assuming you run your dev server on this port
+      : `file://${path.join(__dirname, "../build/index.html")}`,
+  );
+
+
+  mainWindow.on("closed", () => (mainWindow = null));
 }
 
-app.on('ready', createWindow);
+app.on("ready", createWindow);
 
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
 });
 
-app.on('activate', () => {
-    if (mainWindow === null) {
-        createWindow();
-    }
+app.on("activate", () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
 });
